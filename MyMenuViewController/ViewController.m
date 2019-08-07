bc2eaedada5f6b3f890b1de2b508ebb2ea5e861e
zhiyunyu <zhiyunyu@tencent.com>
2019-08-07 21:29:54 +0800

    first commit

--- MyMenuViewController/ViewController.m
+++ MyMenuViewController/ViewController.m
@@ -7,16 +7,95 @@
 //
 
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
 
-@interface ViewController ()
+#define CHECK_VALID_ARRAY(__aArray) (__aArray && [__aArray isKindOfClass:[NSArray class]] && [__aArray count])
 
+
+@interface ViewController ()<WKUIDelegate, WKNavigationDelegate>
+@property(nonatomic, strong) WKWebView *webView;
+@property(nonatomic, strong) UIMenuController* menuController;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    WKWebViewConfiguration *configuration =[[WKWebViewConfiguration alloc] init];
+    self.webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:configuration];
+    [self.view addSubview:self.webView];
+    self.webView.UIDelegate = self;
+    self.webView.navigationDelegate = self;
+    
+    NSURL *url = [[NSURL alloc] initWithString:@"https://view.inews.qq.com/a/20190726A0S73500?uid=100146243534&shareto=wx"];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+    
+    [self.webView loadRequest:request];
+    
+    [self setMenu];
+}
+
+- (void)setMenu {
+    UIMenuController *menuController = [UIMenuController sharedMenuController];
+    [menuController setMenuItems:[self contextMenuItems]];
+    [menuController setMenuVisible:NO];
+    
+    self.menuController = [UIMenuController sharedMenuController];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(willHideMenu:)
+                                                 name:UIMenuControllerWillHideMenuNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(willShowMenu:)
+                                                 name:UIMenuControllerWillShowMenuNotification
+                                               object:nil];
+}
+
+- (void)willShowMenu:(NSNotification*)notification {
+    NSLog(@"willShowMenu");
+}
+
+- (void)willHideMenu:(NSNotification*)notification {
+   NSLog(@"willHideMenu");
+}
+
+
+- (NSArray*)contextMenuItems {
+    static NSArray* menuItems;
+    if (!CHECK_VALID_ARRAY(menuItems)) {
+        NSArray* data = @[
+                          @[@"分享", @"share"],                 //分享
+                          ];
+        NSMutableArray* items = [NSMutableArray array];
+        
+        for (NSArray* pair in data) {
+            NSString* title = pair[0];
+            NSString* selector = pair[1];
+            UIMenuItem* menuItem = [[UIMenuItem alloc] initWithTitle:title action:NSSelectorFromString(selector)];
+            [items addObject:menuItem];
+        }
+        menuItems = items;
+    }
+    return menuItems;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark WKNavigationDelegate
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+    NSURL *url = navigationAction.request.URL;
+    //拦截百度页面
+    if ([url.absoluteString containsString:@"baidu"]) {
+        decisionHandler(WKNavigationActionPolicyCancel);
+    } else {
+        decisionHandler(WKNavigationActionPolicyAllow);
+    }
 }
 
 
